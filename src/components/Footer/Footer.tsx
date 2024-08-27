ab8f48e344c8110f18385f08ce4e55753e864477
AlexProxoor <alex.prokhor03@gmail.com>
2024-08-28 01:49:52 +0300

    фв

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -2,11 +2,13 @@ import React from "react";
 import { FooterContainer } from "./styled";
 
 const Footer: React.FC = () => {
-    return (
-        <FooterContainer>
-            <p>&copy; {new Date().getFullYear()} LaLB. All rights reserved.</p>
-        </FooterContainer>
-    );
+  return (
+    <FooterContainer>
+      <p>
+        &copy; {new Date().getFullYear()} Your Company. All rights reserved.
+      </p>
+    </FooterContainer>
+  );
 };
 
 export default Footer;
